02b4fc6ebfa02ea3b0934a33490b922b2ca66e75
edwin0258 <edwin0258@gmail.com>
2015-09-21 18:27:13 -0700

    Setup calculator functions

---
+++ calculator.js
@@ -0,0 +1,16 @@
+var add = function(x, y){
+	var sum = x + y
+	return sum
+}
+var multiply = function(x, y){
+	var product = x * y
+	return product
+}
+var subtract = function(x, y){
+	var remainder = x - y
+	return remainder
+}
+var divide = function(x, y){
+	var quotient = x / y
+	return quotient
+}
